96e3ea9719554da26b1dc7ac90355c0f8e634d7d
Дмитрий Серов <dima.bessarab@list.ru>
2024-03-24 22:00:47 +0300

    m

--- app/src/main/java/com/example/first/SignInScreen.java
+++ app/src/main/java/com/example/first/SignInScreen.java
@@ -10,20 +10,24 @@ import android.widget.EditText;
 
 public class SignInScreen extends AppCompatActivity {
     Button sign_up;
+    private EditText email;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        email = (EditText) findViewById(R.id.email);
+
+        password = (EditText) findViewById(R.id.password);
+
+        email.setText(getIntent().getStringExtra("name"));
+        password.setText(getIntent().getStringExtra("name2"));
+
     }
     public void GoToSecAct(View v) {
-        EditText email = findViewById(R.id.editText1);
-        EditText password = findViewById(R.id.editText2);
-        String email_str = email.getText().toString();
-        String password_str = password.getText().toString();
-        User user_data = new User(email_str,password_str);
+
         Intent intent = new Intent(this,SignUpScreen.class);
-        intent.putExtra(User.class.getName(),user_data);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/first/SignUpScreen.java
+++ app/src/main/java/com/example/first/SignUpScreen.java
@@ -9,26 +9,25 @@ import android.widget.EditText;
 
 public class SignUpScreen extends AppCompatActivity {
 
+    private EditText emailInput;
+    private  EditText passInput;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
+
+        emailInput = findViewById(R.id.email_sign_up);
+
+        passInput = findViewById(R.id.password_sign_up);
     }
 
     public void GoToFirstAct(View v) {
         Intent intent = new Intent(this,SignInScreen.class);
+        intent.putExtra("name",emailInput.getText().toString());
+        intent.putExtra("name2",passInput.getText().toString());
         startActivity(intent);
-        Data_set();
-    }
-    private void Data_set() {
-        EditText emailInput = findViewById(R.id.editText4);
-        EditText passInput = findViewById(R.id.editText5);
 
-        User user_data = (User) getIntent().getSerializableExtra(User.class.getName());
-
-        if (user_data != null) {
-            emailInput.setText(user_data.Email());
-            passInput.setText(user_data.Password());
-        }
     }
 }
